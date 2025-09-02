451dccc24dd4b96426ee89eb3d04e07662c52f90
kusuma <haranathakusumav@gmail.com>
2025-09-02 10:01:06 +0530

    hi

---
+++ Box.java
@@ -0,0 +1,14 @@
+public class Box{
+int hei=5;
+int wei=25;
+int len=15;
+Box(int a,int b,int c){
+hei=a;
+wei=b;
+len=c;
+}
+public static void main(String[] args){
+Box obj1=new Box(2,3,4);
+System.out.print(obj1.hei);
+}}
+
